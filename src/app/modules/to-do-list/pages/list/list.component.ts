41ad9339be4ed1d817becc884926dee6c5ee646a
Emílio José Camilo <camilojc1982@gmail.com>
2024-03-18 11:32:49 -0300

    Adicionando a visibilidade do button e input com o @if/@else

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-
+  public addItem = signal(true);
 }
